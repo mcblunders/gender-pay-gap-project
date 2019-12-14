ec7c4a21df090bbdeece29dc5cc1e4ac26a6e596
M Hatton <53306899+mcblunders@users.noreply.github.com>
2019-12-14 08:18:26 +0000

    added data transformation scripts

---
+++ Data-transformation/age_industry_transform.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(ggthemes)
+
+gpg_industry <- read_csv("FT_age_industry.csv") %>% 
+  select(Description, Code, GPG_median = `GPG median`, GPG_mean = `GPG mean`) %>% 
+  filter(str_detect(Code,'[:alpha:]')) %>% 
+  separate(Description,into = c("Age_range", "Industry"), sep = ",  ") %>% 
+  mutate(GPG_median = na_if(GPG_median,"x"),
+         GPG_mean = na_if(GPG_mean,"x")) %>% 
+  mutate(GPG_median = as.numeric(GPG_median),
+         GPG_mean = as.numeric(GPG_mean)) %>% 
+  select(-Code) %>% 
+  filter(!is.na(GPG_median))
+gpg_industry %>% write_csv('gpg_industry.csv')
